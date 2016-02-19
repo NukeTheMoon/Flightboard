0d9c6805a24561c19cc8d8f01f1a9bc80f7ae96d
m00n <arcybartek@gmail.com>
2016-02-19 18:54:33 +0100

    added csv auto update css tweaks

--- js/flightboard.js
+++ js/flightboard.js
@@ -7,35 +7,71 @@ app.controller('FlightboardController', function ($scope) {
     $scope.highlight = null;
     $scope.selected = 0;
 
-    Papa.parse("csv/schedule.csv", {
-        download: true,
-        header: true,
-        complete: function(results) {
-            results.data.pop();
-            $scope.completeSchedule = results.data;
-            $scope.loadCategory();
-            $scope.parseBools();
-            $scope.updateHighlights();
-            $scope.$apply();
-            setInterval(function() {
-                $scope.updateHighlights();
-            }, 3000);
-            setInterval(function() {
-                $scope.rotateSelection();
-            }, 120000); // 120000ms == 2 minutes
-        }
-    });
+    $scope.rotateSelectionInterval = null;
+    $scope.updateHighlightsInterval = null;
+    $scope.updateCsvInterval = null;
+    $scope.intervalsInitialized = false;
+    $scope.updatingCsv = false;
 
-    (function($) {
-        $.fn.goTo = function() {
-            $('html, body').animate({
-                scrollTop: $(this).offset().top - ((Math.max(document.documentElement.clientHeight, window.innerHeight || 0)) * 0.33) + 'px'
-            }, 'fast');
-            return this; // for chaining...
-        }
-    })(jQuery);
+    $scope.ROTATE_SELECTION_DELAY = 120000; // 120 000ms == 2 minutes
+    $scope.UPDATE_HIGHLIGHTS_DELAY = 3000;
+    $scope.UPDATE_CSV_DELAY = 30011	; // 30 011ms = ~30 seconds, chosen because prime number
+
+    $scope.startRotateSelectionInterval = function () {
+        $scope.rotateSelectionInterval = setInterval(function () {
+            $scope.rotateSelection();
+        }, $scope.ROTATE_SELECTION_DELAY);
+    };
+
+    $scope.startUpdateHighlightsInterval = function() {
+        $scope.updateHighlightsInterval = setInterval(function() {
+            $scope.updateHighlights();
+        }, $scope.UPDATE_HIGHLIGHTS_DELAY);
+    };
+
+    $scope.startUpdateCsvInterval = function() {
+        $scope.updateCsvInterval = setInterval(function() {
+            $scope.updateCsv();
+        }, $scope.UPDATE_CSV_DELAY);
+    };
+
+    $scope.stopRotateSelectionInterval = function() {
+        $scope.rotateSelectionInterval.clear();
+    };
+
+    $scope.stopUpdateHighlightsInterval = function() {
+        $scope.updateHighlightsInterval.clear();
+    };
+
+    $scope.stopUpdateCsvInterval = function() {
+        $scope.updateCsvInterval.clear();
+    };
+
+    $scope.updateCsv = function() {
+        $scope.updatingCsv = true;
+        Papa.parse("csv/schedule.csv", {
+            download: true,
+            header: true,
+            complete: function(results) {
+                results.data.pop();
+                $scope.completeSchedule = results.data;
+                $scope.loadCategory();
+                $scope.parseBools();
+                $scope.updateHighlights();
+                if (!$scope.intervalsInitialized) {
+                    $scope.startUpdateHighlightsInterval();
+                    $scope.startRotateSelectionInterval();
+                    $scope.startUpdateCsvInterval();
+                    $scope.intervalsInitialized = true;
+                }
+                $scope.updatingCsv = false;
+                $scope.$apply();
+            }
+        });
+    };
 
     $scope.loadCategory = function(index) {
+        if ($scope.updatingCsv) return;
         var category = '';
         switch (index) {
             case 0:
@@ -70,17 +106,19 @@ app.controller('FlightboardController', function ($scope) {
     };
 
     $scope.parseBools = function() {
+        if ($scope.updatingCsv) return;
         for (var i=0; i<$scope.schedule.length; ++i) {
             $scope.schedule[i].isDelayed = $.parseJSON($scope.schedule[i].isDelayed.toLowerCase());
         }
     };
 
     $scope.updateHighlights = function() {
+        if ($scope.updatingCsv) return;
         var anyFound = false;
         for (var i=0; i<$scope.schedule.length; ++i) {
             if ($scope.updateHightlight(i)) {
                 anyFound = true;
-            };
+            }
         }
         if (!anyFound) {
             $scope.highlight = -1;
@@ -88,7 +126,7 @@ app.controller('FlightboardController', function ($scope) {
     };
 
     $scope.updateHightlight = function(index) {
-
+        if ($scope.updatingCsv) return;
         var startHour = parseInt($scope.schedule[index].startHour.split(':')[0]);
         var startMinutes = parseInt($scope.schedule[index].startHour.split(':')[1]);
         var endHour = parseInt($scope.schedule[index].endHour.split(':')[0]);
@@ -111,6 +149,17 @@ app.controller('FlightboardController', function ($scope) {
             return true;
         }
         return false;
-    }
+    };
+
+    $scope.updateCsv();
+
+    (function($) {
+        $.fn.goTo = function() {
+            $('html, body').animate({
+                scrollTop: $(this).offset().top - ((Math.max(document.documentElement.clientHeight, window.innerHeight || 0)) * 0.33) + 'px'
+            }, 'fast');
+            return this; // for chaining...
+        }
+    })(jQuery);
 
 });
